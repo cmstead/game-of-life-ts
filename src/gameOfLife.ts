30ee8403464fcd1004fcf9f6ee544336e7888268
Chris Stead <cmstead@gmail.com>
2022-04-21 11:23:01 -0700

    F add rule 2 neighbours cell survives

--- src/gameOfLife.ts
+++ src/gameOfLife.ts
@@ -1,8 +1,45 @@
 export default {
     board: [[' ']],
-    run() {
-      this.board[0][0] = ' '  
+    init(x, y) {
+        for (let i = 0; i < x; i++) {
+            this.board[i] = [];
+            for (let j = 0; j < y; j++) {
+                this.board[i][j] = ' ';
+            }
+        }
     },
+    run() {
+        for (const rowIndex in this.board) {
+            for(const cellIndex in this.board[rowIndex]) {
+                const cellIndexInt = parseInt(cellIndex)
+                const rowIndexInt = parseInt(rowIndex)
+                // do not check for dead cells
+                if (this.board[rowIndexInt][cellIndexInt] !== 'o') {
+                    continue;
+                }
+                let numberOfLiveNeighbours = 0;
+                if (rowIndexInt < this.board.length - 1 && this.board[rowIndexInt + 1][cellIndexInt] === 'o') {
+                    numberOfLiveNeighbours++;
+                }
+                if (this.board[rowIndexInt][cellIndexInt + 1] === 'o') {
+                    numberOfLiveNeighbours++;
+                }
+                
+                if(numberOfLiveNeighbours < 2) {
+                    this.board[rowIndexInt][cellIndexInt] = ' ';
+                }
+
+            }
+        }
+    },
+    setCell(x, y, value) {
+        if (!this.board[x]) {
+            this.board[x] = [];
+        }
+
+        this.board[x][y] = value;
+    },
+
     getBoard() {
         return this.board
     },
--- tests/gameOfLife.test.ts
+++ tests/gameOfLife.test.ts
@@ -13,4 +13,16 @@ describe('Game Of Life Rules', () => {
         gameOfLife.run();
         expect(board[0][0]).toBe(' ');
     })
+
+    it('survives when cell has two nieghbours', () => {
+        gameOfLife.init(2, 2);
+        const board = gameOfLife.getBoard();
+        gameOfLife.setCell(0, 0, 'o');
+        gameOfLife.setCell(0, 1, 'o');
+        gameOfLife.setCell(1, 0, 'o');
+
+        gameOfLife.run()
+        
+        expect(board[0][0]).toBe('o');
+    })
 })
